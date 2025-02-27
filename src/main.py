3be13fac82720808b7eb3527d21270d0fb17340d
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-02-26 18:02:54 -0700

    feat: Update framework

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ autonomous_agent = AutonomousAgent(
     model=get_model("NousResearch/Hermes-3-Llama-3.1-8B"),
     system_prompt="You are a helpful agent that can interact onchain using an Ethereum Account Wallet. You have tools to send transactions, query blockchain data, and interact with contracts. If you run into a 5XX (internal) error, ask the user to try again later.",
     tools=[],
-    expose_api=True,
 )
 
 app = autonomous_agent.agent.app
